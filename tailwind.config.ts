331cba867ac4ee8040676d83eb06843851c05be6
SaagarRaj <saagartiwari1992@gmail.com>
2024-08-02 20:42:25 -0700

    Initial project setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,32 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        'custom-blue': '#001537',
+        'custom-yellow': '#FCA311',
+        'custom-offWhite': '#E5E5E5',
+      },
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+        Poppins: ['Poppins', 'sans-serif'],
+        robotoMono: ['Roboto Mono', 'monospace'],
+      },
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+      },
+      boxShadow: {
+        'black-shadow':
+          '0 0 10px rgba(0, 0, 0, 0.7), 0 0 20px rgba(0, 0, 0, 0.5), 0 0 30px rgba(0, 0, 0, 0.3)',
       },
     },
   },
